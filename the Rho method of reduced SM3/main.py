1ec0f03c21a1b6dfe7d5e4d23df393fdf84ba2c6
lxf0728 <109843978+lxf0728@users.noreply.github.com>
2022-07-25 01:42:44 +0800

    Add files via upload

---
+++ the Rho method of reduced SM3/main.py
@@ -0,0 +1,36 @@
+from gmssl import sm3, func
+import random
+#成功找到了40位的碰撞,数据对如下：
+#b'2aed0f8289',b'e7ab762d2f'
+x_0 = str(random.randint(0, 2**41-1))#l+1
+n=10
+x_0 = bytes(x_0, encoding='utf-8')
+x_1 = sm3.sm3_hash(func.bytes_to_list(x_0))
+x_2 = bytes(x_1[0:n], encoding='utf-8')
+x_2 = sm3.sm3_hash(func.bytes_to_list(x_2))
+i=0
+
+while x_1[0:n] != x_2[0:n]:#寻找32位的碰撞
+        x_1 = bytes(x_1[0:n], encoding='utf-8')
+        x_2 = bytes(x_2[0:n], encoding='utf-8')
+        x_1 = sm3.sm3_hash(func.bytes_to_list(x_1))
+        x_2 = sm3.sm3_hash(func.bytes_to_list(x_2))
+        x_2 = bytes(x_2[0:n], encoding='utf-8')
+        x_2 = sm3.sm3_hash(func.bytes_to_list(x_2))
+        i+=1
+
+print(i)
+
+x_2=x_1
+x_1 = x_0
+x_2 = bytes(x_2[0:n], encoding='utf-8')
+
+for j in range(i):
+    if sm3.sm3_hash(func.bytes_to_list(x_1))[0:n] == sm3.sm3_hash(func.bytes_to_list(x_2))[0:n]:
+        print(x_1,x_2,sm3.sm3_hash(func.bytes_to_list(x_2))[:n])
+        break
+    else:
+        x_1 = sm3.sm3_hash(func.bytes_to_list(x_1))
+        x_1 = bytes(x_1[0:n], encoding='utf-8')
+        x_2 = sm3.sm3_hash(func.bytes_to_list(x_2))
+        x_2 = bytes(x_2[0:n], encoding='utf-8')
